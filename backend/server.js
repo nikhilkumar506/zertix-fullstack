336389966e85cf3f9294052c6051151e3dfc5727
nikhilKumar506 <nikhilpal04022@gmail.com>
2026-04-04 14:08:25 +0530

    final: connect frontend with backend

--- backend/server.js
+++ backend/server.js
@@ -8,3 +8,12 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
   console.log(`🚀 Server running on port ${PORT}`)
 );
+const path = require("path");
+
+// serve frontend static files
+app.use(express.static(path.join(__dirname, "frontend")));
+
+// homepage route
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend/index.html"));
+});
