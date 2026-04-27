e7ae4b42d8f00d0e386a20d474268efae282a082
nikhilKumar506 <nikhilpal04022@gmail.com>
2026-04-27 10:30:35 +0530

    Added Notes Section (Class-wise + Subjects)

---
+++ backend/frontend/data/notesData.js
@@ -0,0 +1,37 @@
+const notesData = {
+  "Class 6": [
+    "Hindi", "English", "Math", "Science",
+    "Social Science", "Sanskrit",
+    "Hindi Grammar", "GK", "Art"
+  ],
+
+  "Class 7": [
+    "Hindi", "English", "Math", "Science",
+    "Social Science", "Sanskrit", "GK"
+  ],
+
+  "Class 8": [
+    "Hindi", "English", "Math", "Science",
+    "Social Science", "Sanskrit", "GK"
+  ],
+
+  "Class 9": [
+    "Hindi", "English", "Math", "Science",
+    "Social Science"
+  ],
+
+  "Class 10": [
+    "Hindi", "English", "Math",
+    "Science", "Computer", "Art"
+  ],
+
+  "Class 11": [
+    "Hindi", "English", "Physics",
+    "Chemistry", "Math", "Biology"
+  ],
+
+  "Class 12": [
+    "Hindi", "English", "Physics",
+    "Chemistry", "Math", "Biology"
+  ]
+};
