3ad8464ae562bc2f203b0c04d0e7f4b6357d5ebc
nikhilKumar506 <nikhilpal04022@gmail.com>
2026-05-09 14:58:47 +0530

    Fixed resume routes

--- backend/src/routes/resumeRoutes.js
+++ backend/src/routes/resumeRoutes.js
@@ -2,33 +2,6 @@ const express = require("express");
 
 const router = express.Router();
 
-const {
-  createResume,
-  getAllResumes,
-  getResumeById,
-  updateResume,
-  deleteResume
-} = require("../controllers/resumeController");
-
-/* ================= CREATE ================= */
-router.post("/", createResume);
-
-/* ================= GET ALL ================= */
-router.get("/", getAllResumes);
-
-/* ================= GET SINGLE ================= */
-router.get("/:id", getResumeById);
-
-/* ================= UPDATE ================= */
-router.put("/:id", updateResume);
-
-/* ================= DELETE ================= */
-router.delete("/:id", deleteResume);
-
-module.exports = router;const express = require("express");
-
-const router = express.Router();
-
 const multer = require("multer");
 
 const upload = multer({
@@ -47,21 +20,26 @@ const {
 } = require("../controllers/resumeController");
 
 /* ================= CREATE ================= */
+
 router.post("/", createResume);
 
 /* ================= GET ALL ================= */
+
 router.get("/", getAllResumes);
 
 /* ================= GET SINGLE ================= */
+
 router.get("/:id", getResumeById);
 
 /* ================= UPDATE ================= */
+
 router.put("/:id", updateResume);
 
 /* ================= DELETE ================= */
+
 router.delete("/:id", deleteResume);
 
-/* ================= UPLOAD + PARSE ================= */
+/* ================= UPLOAD RESUME ================= */
 
 router.post(
   "/upload",
